ca1f4ec25f8dc9a4530b054ee724adcb57631df5
taku-y <taku.yoshioka.4096@gmail.com>
2018-12-09 01:01:04 +0900

    Add matplotlib

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ source q-trader-env/bin/activate
 pip install tensorflow==1.12
 pip install keras==2.2.4
 pip install gym==0.10.9
+pip install matplotlib==3.0.2
 # pip install ruamel-yaml==0.15.76d
 # pip install stable-baselines==2.2.1
