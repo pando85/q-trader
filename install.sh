976e903c97e67a48cfd3846ae431e57004c830d5
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2018-12-08 13:28:40 +0900

    Add script for installing python venv

---
+++ install.sh
@@ -0,0 +1,7 @@
+python3 -m venv q-trader-env
+source q-trader-env/bin/activate
+pip install tensorflow==1.12
+pip install keras==2.2.4
+pip install gym==0.10.9
+# pip install ruamel-yaml==0.15.76d
+# pip install stable-baselines==2.2.1
