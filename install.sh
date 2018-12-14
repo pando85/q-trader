39d76f8560382f52a1793cd630e09f6a8c13e426
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2018-12-14 20:33:15 +0900

    Install tabulate 0.8.2

--- install.sh
+++ install.sh
@@ -4,5 +4,6 @@ pip install tensorflow==1.12
 pip install keras==2.2.4
 pip install gym==0.10.9
 pip install matplotlib==3.0.2
+pip install tabulate==0.8.2
 # pip install ruamel-yaml==0.15.76d
 # pip install stable-baselines==2.2.1
