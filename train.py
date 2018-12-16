5d5c7e41b06810716ca5b41f2dde3634a103eb42
taku-y <taku.yoshioka.4096@gmail.com>
2018-12-17 00:07:24 +0900

    Modify learning frequency

--- train.py
+++ train.py
@@ -13,16 +13,18 @@ stock_name, window_size, episode_count = sys.argv[1], int(sys.argv[2]), int(
 batch_size = 32
 agent = Agent(window_size)
 
+# Environment
 env = SimpleTradeEnv(stock_name, window_size, agent)
 
+# Loop over episodes
 for e in range(episode_count + 1):
+  # Initialization before starting an episode
   print("Episode " + str(e) + "/" + str(episode_count))
   state = env.reset()
-
   agent.inventory = []
   done = False
 
-  # An episode
+  # Loop in an episode
   while not done:
     action = agent.act(state)
     next_state, reward, done, _ = env.step(action)
@@ -34,8 +36,8 @@ for e in range(episode_count + 1):
       print("Total Profit: " + formatPrice(env.total_profit))
       print("--------------------------------")
 
-    if len(agent.memory) > batch_size:
-      agent.expReplay(batch_size)
+  if len(agent.memory) > batch_size:
+    agent.expReplay(batch_size)
 
   if e % 10 == 0:
     agent.model.save("models/model_ep" + str(e))
