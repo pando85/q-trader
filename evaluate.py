25ef3b7fc45838bd9402b34070086775fe9524c4
Taku Yoshioka <taku.yoshioka.4096@gmail.com>
2018-12-19 20:00:43 +0900

    Refactor evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -1,66 +1,56 @@
-import keras
-import matplotlib.pyplot as plt
-from keras.models import load_model
-
-from agent.agent import Agent
-from functions import *
 import sys
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.models import load_model
+from agent.agent import Agent
+from environment import SimpleTradeEnv
+from functions import formatPrice
 
 if len(sys.argv) != 3:
 	print("Usage: python evaluate.py [stock] [model]")
 	exit()
 
 stock_name, model_name = sys.argv[1], sys.argv[2]
+
+# Load agent
 model = load_model("models/" + model_name)
-window_size = model.layers[0].input.shape.as_list()[1]
-
+window_size = model.layers[0].input.shape.as_list()[1] - 1
 agent = Agent(window_size, True, model_name)
-data = getStockDataVec(stock_name)
-l = len(data) - 1
-batch_size = 32
 
-state = getState(data, 0, window_size + 1)
-total_profit = 0
+# Environment
+env = SimpleTradeEnv(stock_name, window_size, agent)
+
+# Initialization before starting an episode
+state = env.reset()
 agent.inventory = []
+done = False
 
+# Loop in an episode
+t = 0
 ts_buy = []
 ts_sell = []
 
-for t in range(l):
+while not done:
+	could_buy = len(agent.inventory) > 0
 	action = agent.act(state)
+	next_state, _, done, _ = env.step(action)
+	state = next_state
 
-	# sit
-	next_state = getState(data, t + 1, window_size + 1)
-	reward = 0
-
-	if action == 1: # buy
-		agent.inventory.append(data[t])
-		print("Buy: " + formatPrice(data[t]))
+	if action == 1:
 		ts_buy.append(t)
-
-	elif action == 2 and len(agent.inventory) > 0: # sell
-		bought_price = agent.inventory.pop(0)
-		reward = max(data[t] - bought_price, 0)
-		total_profit += data[t] - bought_price
-		print("Sell: " + formatPrice(data[t]) + " | Profit: " +
-					formatPrice(data[t] - bought_price))
+	elif action == 2 and could_buy:
 		ts_sell.append(t)
 
-	else:
-		print("Sit")
-
-	done = True if t == l - 1 else False
-	agent.memory.append((state, action, reward, next_state, done))
-	state = next_state
+	t += 1
 
 	if done:
 		print("--------------------------------")
-		print(stock_name + " Total Profit: " + formatPrice(total_profit))
+		print("Total Profit: " + formatPrice(env.total_profit))
 		print("--------------------------------")
 
 plt.figure()
-data = np.array(data)
-ts = np.arange(l).astype(int)
+data = np.array(env.data)
+ts = np.arange(len(env.data)).astype(int)
 ts_buy = np.array(ts_buy).astype(int)
 ts_sell = np.array(ts_sell).astype(int)
 plt.plot(ts, data[ts])
